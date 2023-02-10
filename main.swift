115b0e20d684a190a56fdb9fb2b008c43ba659e7
David Inga <davidinga@gmail.com>
2023-02-10 19:32:46 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,60 @@
+/*
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+Example 1:
+
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-1000 <= Node.val <= 1000
+*/
+
+func serialize(_ root: TreeNode?) -> String {
+    var result = ""
+
+    func preOrderTraversal(_ root: TreeNode?) {
+        guard let root = root else { 
+            result.append(",nil") 
+            return
+        }
+        result.append(",\(root.val)")
+        preOrderTraversal(root.left)
+        preOrderTraversal(root.right)
+    }
+
+    preOrderTraversal(root)
+
+    return result
+}
+
+func deserialize(_ data: String) -> TreeNode? {
+    guard !data.isEmpty else { return nil }
+
+    var data = data
+        .split(separator: ",")
+        .makeIterator()
+
+    func preOrderTraversal(_ value: String.SubSequence) -> TreeNode? {
+        guard let value = Int(value) else { return nil }
+
+        let root = TreeNode(value)
+        root.left = preOrderTraversal(data.next()!)
+        root.right = preOrderTraversal(data.next()!)
+
+        return root
+    }
+
+    return preOrderTraversal(data.next()!)
+}
